01b0ae26898914090e1bf97d004b7ac5d2e6032b
Aly <alyshariff2810@gmail.com>
2024-05-21 19:11:03 -0400

    new changes

---
+++ main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b5c31e018820cffa08599d5a8444861f7639e57005f053ba8dc83b994332848b
+size 1251
--- src/config.py
+++ src/config.py
@@ -1,6 +1,3 @@
-config = {
-	"api_type": "azure",
-	"api_base": "YOUR_API_BASE_URL",
-	"api_version": "2023-07-01-preview",
-	"api_key": "YOUR_API_KEY"
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:dc2c128fc38242e1f2c28efdecb59759482a916eba085a7047b5cc06a7a168ed
+size 177
