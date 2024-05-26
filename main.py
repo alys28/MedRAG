156898007a4585a90641ae311fbd59d93b61d02e
Aly <alyshariff2810@gmail.com>
2024-05-25 23:44:50 -0400

    fixing issues with env

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4b1821691dac2d09d33dcea17dbf48007961ebc19c5d573578fc0976bd5215be
-size 1277
+from src.medrag import MedRAG
+
+question = "A lesion causing compression of the facial nerve at the stylomastoid foramen will cause ipsilateral"
+options = {
+    "A": "paralysis of the facial muscles.",
+    "B": "paralysis of the facial muscles and loss of taste.",
+    "C": "paralysis of the facial muscles, loss of taste and lacrimation.",
+    "D": "paralysis of the facial muscles, loss of taste, lacrimation and decreased salivation."
+}
+
+## CoT Prompting
+cot = MedRAG(llm_name="OpenAI/gpt-3.5-turbo", rag=True, retriever_name="BM25")
+# answer, _, _ = cot.answer(question=question, options=options)
+# print(answer)
+# from openai import OpenAI
+# import json
+# import os
+# client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY", "..."))
+# # Example prompt and completion request
+# MODEL = "gpt-3.5-turbo"
+# response = client.chat.completions.create(
+#     model=MODEL,
+#     messages=[
+#         {"role": "system", "content": "You are a helpful assistant."},
+#         {"role": "user", "content": "Knock knock."},
+#         {"role": "assistant", "content": "Who's there?"},
+#         {"role": "user", "content": "Orange."},
+#     ],
+#     temperature=0,
+# )
+# print(json.dumps(json.loads(response.model_dump_json()), indent=4))
+
--- src/config.py
+++ src/config.py
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:dc2c128fc38242e1f2c28efdecb59759482a916eba085a7047b5cc06a7a168ed
-size 177
+config = {
+	"api_type": "azure",
+	"api_base": "https://api.openai.com",
+	"api_version": "2023-06-01-preview",
+	"api_key": "..."
+}
