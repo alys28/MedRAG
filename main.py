b1334b017a3e146a8fbdc012059b8fc381431589
Aly <alyshariff2810@gmail.com>
2024-05-25 23:21:39 -0400

    started building RAG system

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5c31e018820cffa08599d5a8444861f7639e57005f053ba8dc83b994332848b
-size 1251
+oid sha256:4b1821691dac2d09d33dcea17dbf48007961ebc19c5d573578fc0976bd5215be
+size 1277
--- src/data/textbooks.py
+++ src/data/textbooks.py
@@ -1,31 +1,3 @@
-import os
-import tqdm
-import json
-import re
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-def ends_with_ending_punctuation(s):
-    ending_punctuation = ('.', '?', '!')
-    return any(s.endswith(char) for char in ending_punctuation)
-
-def concat(title, content):
-    if ends_with_ending_punctuation(title.strip()):
-        return title.strip() + " " + content.strip()
-    else:
-        return title.strip() + ". " + content.strip()
-
-if __name__ == "__main__":
-
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    fdir = "corpus/textbooks/en"
-    fnames = sorted(os.listdir(fdir))
-
-    if not os.path.exists("corpus/textbooks/chunk"):
-        os.makedirs("corpus/textbooks/chunk")
-        
-    for fname in tqdm.tqdm(fnames):
-        fpath = os.path.join("corpus/textbooks/en", fname)
-        texts = text_splitter.split_text(open(fpath).read().strip())
-        saved_text = [json.dumps({"id": '_'.join([fname.replace(".txt", ''), str(i)]), "title": fname.strip(".txt"), "content": re.sub("\s+", " ", texts[i]), "contents": concat(fname.strip(".txt"), re.sub("\s+", " ", texts[i]))}) for i in range(len(texts))]
-        with open("corpus/textbooks/chunk/{:s}".format(fname.replace(".txt", ".jsonl")), 'w') as f:
-            f.write('\n'.join(saved_text))
+version https://git-lfs.github.com/spec/v1
+oid sha256:aa93142c99aa034e3526f35e038a435b2193c9dccdde328a90d3ae3958428af4
+size 1312
